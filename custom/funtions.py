b1a752ea90f45a5f33a3370abbfdbb0276142f9d
Gaston <cutkillerone@gmail.com>
2023-02-25 13:01:00 -0300

    update
    conversion y creacion de nueva columna estado.
    identificacion X,Y

--- custom/funtions.py
+++ custom/funtions.py
@@ -1,2 +1,10 @@
+#suma los numeros de una lista
 def suma(a: list[int]):
     return sum(a)
+
+#Buscando a los aprobados y dandoles un valor 1, todo lo demas quedaria en 0 (Reprobado) y creamos una nueva columna para este valor nuevo.
+def set_int_aprobado(estado ):
+    if(estado == 'A'):
+        return 1
+    else:
+        return 0
